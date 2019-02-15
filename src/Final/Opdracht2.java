43f8bb2cff83103824517aa7f3831f8757c45459
Sander Cokart <112039@student.drenthecollege.nl>
2019-02-15 12:29:26 +0100

    bugfix

--- src/Final/Opdracht2.java
+++ src/Final/Opdracht2.java
@@ -1,11 +1,8 @@
 package Final;
 
 public class Opdracht2 {
-	/**
-	 * @param args the command line arguments
-	 */
+	
 	public static void main(String[] args) {
-		char[] weg = { '"', '?' };
 
 		String tekst = "Dit is een tekst met \" en ** en ?? "
 				+ "en allerlei andere niet wenselijke tekens zoals ® etc. ";
